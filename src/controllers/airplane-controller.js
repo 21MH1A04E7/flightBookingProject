d80dab0788e2d3d44a47d2ec09f291c83dfcc3ce
hariom <hk524569@gmail.com>
2025-08-09 20:23:49 +0530

    fix: joi validation for the airplane

--- src/controllers/airplane-controller.js
+++ src/controllers/airplane-controller.js
@@ -10,15 +10,16 @@ async function createAirplane(req,res) {
             modelNumber:Joi.string().min(3).max(25).required(),
             capacity:Joi.number().integer().min(1).required()
         })
-        const {error}=airplaneSchema.validate(req.body)
+        const {error,value}=airplaneSchema.validate(req.body)
         if(error){
+            console.log('Validation Error ',error)
             ErrorResponse.message = error?.details[0]?.message;
-            ErrorResponse.error = error?.details[0]?.message;
+            ErrorResponse.error = error.details.map(d => d.message);
             return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
         }
         const airplane=await AirplaneService.createAirplane({
-            modelNumber:req.body.modelNumber,
-            capacity:req.body.capacity
+            modelNumber:value.modelNumber,
+            capacity:value.capacity
         })
         SuccessResponse.data=airplane
         return res.status(StatusCodes.CREATED)
