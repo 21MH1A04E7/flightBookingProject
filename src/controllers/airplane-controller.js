19fa9735a13a517db96a52b1d1080d08896cfc03
hariom <hk524569@gmail.com>
2025-08-09 20:14:53 +0530

    worked on the joi validation

--- src/controllers/airplane-controller.js
+++ src/controllers/airplane-controller.js
@@ -1,10 +1,21 @@
 const {AirplaneService}=require('../services')
 const { StatusCodes } = require('http-status-codes');
 const {ErrorResponse,SuccessResponse}=require('../utils/common')
+const Joi = require('joi');
 
 async function createAirplane(req,res) {
     try{
         
+        const airplaneSchema=Joi.object({
+            modelNumber:Joi.string().min(3).max(25).required(),
+            capacity:Joi.number().integer().min(1).required()
+        })
+        const {error}=airplaneSchema.validate(req.body)
+        if(error){
+            ErrorResponse.message = error?.details[0]?.message;
+            ErrorResponse.error = error?.details[0]?.message;
+            return res.status(StatusCodes.BAD_REQUEST).json(ErrorResponse);
+        }
         const airplane=await AirplaneService.createAirplane({
             modelNumber:req.body.modelNumber,
             capacity:req.body.capacity
